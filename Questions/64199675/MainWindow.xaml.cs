6dcdc0b996af88e5167c0b2c4b0537a439b5dcfb
Vasiliy <popov.vasilij@gmail.com>
2020-10-05 00:27:12 +0300

    Added 64199675

---
+++ Questions/64199675/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MyMethod();
+        }
+
+private void MyMethod()
+{
+    ImageSource   StageWindowImageSource = img.Source;
+    CroppedBitmap Crop                   = new CroppedBitmap((BitmapSource) StageWindowImageSource, new Int32Rect(20, 20, 150, 110));
+
+    img2.Source = GetImage(Crop, new JpegBitmapEncoder());
+}
+
+private BitmapImage GetImage(BitmapSource source, BitmapEncoder encoder)
+{
+    var image = new BitmapImage();
+
+    using (var sourceMs = new MemoryStream())
+    {
+        encoder.Frames.Add(BitmapFrame.Create(source));
+        encoder.Save(sourceMs);
+
+        sourceMs.Position = 0;
+        using (var destMs = new MemoryStream(sourceMs.ToArray()))
+        {
+            image.BeginInit();
+            image.StreamSource = destMs;
+            image.CacheOption  = BitmapCacheOption.OnLoad;
+            image.EndInit();
+            image.Freeze();
+        }
+    }
+
+    return image;
+}
+    }
+}
