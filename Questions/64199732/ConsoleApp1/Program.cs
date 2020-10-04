ae94f4ec166dc6a7b7cf1a87a6479fee1b8d1249
Vasiliy <popov.vasilij@gmail.com>
2020-10-05 00:57:11 +0300

    Added 64199732

---
+++ Questions/64199732/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = " text\r\n\r\n\r\n  spaces    ok  \r\n  yeap.  \r\n";
+            string expected = "text\r\nspaces ok\r\nyeap.";
+
+            string result = string.Join("\r\n", input.Split("\r\n", StringSplitOptions.RemoveEmptyEntries).Select(x => Regex.Replace(x.Trim(), @"\s+", " ")));
+            // string result = Regex.Replace(input, @"[\r\n]+", "\n");
+            // result = Regex.Replace(result, @"(?:(?![\r\n])\s)+", " ");
+            Console.WriteLine($"|{expected}|");
+            Console.WriteLine($"|{result}|");
+            Console.WriteLine();
+            Console.WriteLine(result == expected);
+        }
+    }
+}
